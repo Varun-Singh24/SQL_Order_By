5af9df4d3759264d801858fcc4f336240b749497
Varun Pratap Singh <146599301+Varun-Singh24@users.noreply.github.com>
2025-12-11 12:34:44 +0530

    Order by SQL

---
+++ Q5_Order_BY.sql
@@ -0,0 +1,15 @@
+USE employees ; 
+
+select * from Employees ; 
+
+select * from Employees order by age ;  -- ASC 
+
+select * from Employees order by age asc ;  -- ASCENDING 
+
+select * from Employees order by age desc ;  -- DECENDING 
+
+select * from Employees order by department ;
+
+select * from Employees order by age desc, name ;
+
+select * from Employees order by age desc, employee_id asc ;
